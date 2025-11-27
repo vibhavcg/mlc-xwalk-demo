bc67327dfd0eaaa3fbdff64e7e8b23b6f0a56798
Vibhav Kumar <vibhav.kumar@capgemini.com>
2025-11-27 16:35:16 +1100

    mlc design migration from da.live to AEM

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -6,16 +6,21 @@ export default function decorate(block) {
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
+    // Preserve instrumentation from row → li
     moveInstrumentation(row, li);
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
+      if (div.children.length === 1 && div.querySelector('picture')) {
+        div.className = 'cards-card-image';
+      } else {
+        div.className = 'cards-card-body';
+      }
     });
     ul.append(li);
   });
   ul.querySelectorAll('picture > img').forEach((img) => {
     const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
+    // Preserve instrumentation from old <img> → new optimized <img>
     moveInstrumentation(img, optimizedPic.querySelector('img'));
     img.closest('picture').replaceWith(optimizedPic);
   });
