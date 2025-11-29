1a062b89a5bfe4ff3cf5fde104da1bd68a6a3698
Vibhav Kumar <vibhav.kumar@capgemini.com>
2025-11-29 14:21:54 +1100

    Resolved Lint issue

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,48 +1,48 @@
-import { buildBlock, createOptimizedPicture } from "../../scripts/aem.js";
+import { buildBlock } from '../../scripts/aem.js';
 
 export default function decorate(block) {
-  const heroBannerBlock = [];
+  // const heroBannerBlock = [];
   const blockClassList = block.classList;
 
   // Create a new div for the hero banner
   // and append the first child of the block to it
-  const heroBanner = document.createElement("div");
-  heroBanner.classList.add("hero-banner");
+  const heroBanner = document.createElement('div');
+  heroBanner.classList.add('hero-banner');
   heroBanner.classList.add(...blockClassList);
-  
+
   heroBanner.append(block.firstElementChild);
-  heroBanner.firstElementChild.classList.add("hero-banner--elements");
+  heroBanner.firstElementChild.classList.add('hero-banner--elements');
 
   // Add a class to the first child picture element
-  const firstPicture = heroBanner.firstElementChild.querySelector("picture");
+  const firstPicture = heroBanner.firstElementChild.querySelector('picture');
   if (firstPicture) {
-    firstPicture.classList.add("hero-banner--picture");
+    firstPicture.classList.add('hero-banner--picture');
   }
 
   // Create a new div for the hero overlay
-  const heroBannerTexts = document.createElement("div");
-  heroBannerTexts.classList.add("hero-overlay");
+  const heroBannerTexts = document.createElement('div');
+  heroBannerTexts.classList.add('hero-overlay');
 
   // Move overlay content into heroBannerTexts
   [...block.children].slice(0, 3).forEach((child) => {
     if (child) {
-      if (!child.querySelector("picture") && child.querySelector("p")) {
-        child.classList.add("hero-overlay--text");
+      if (!child.querySelector('picture') && child.querySelector('p')) {
+        child.classList.add('hero-overlay--text');
       }
       heroBannerTexts.append(child);
     }
   });
 
   const heroBannerElements = heroBanner.querySelector(
-    ".hero-banner--elements"
+    '.hero-banner--elements',
   );
 
   // Append overlay as sibling to picture inside hero-banner--elements
   if (heroBannerElements) {
     // If picture exists, insert overlay after it
-    const picture = heroBannerElements.querySelector("picture");
+    const picture = heroBannerElements.querySelector('picture');
     if (picture) {
-      picture.insertAdjacentElement("afterend", heroBannerTexts);
+      picture.insertAdjacentElement('afterend', heroBannerTexts);
     } else {
       heroBannerElements.append(heroBannerTexts);
     }
@@ -51,7 +51,7 @@ export default function decorate(block) {
   heroBanner.append(heroBannerElements);
 
   // create new block with the heroBanner
-  const heroOverlayBlock = buildBlock("hero-banner-block", {
+  const heroOverlayBlock = buildBlock('hero-banner-block', {
     elems: [heroBanner],
   });
 
